743448e259d1a3d348fe02652cb4d8a3dd31b334
root <bentegrimohamed@gmail.com>
2024-07-13 19:10:25 +0000

    fixing taks1

--- markdown2html.py
+++ markdown2html.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
+"""
+markdown2html module
+Converts a Markdown file to HTML.
+"""
 
 import sys
-import os.path
-"""Making a file that converts markdown to html
-    """
+import os
+import markdown
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
+def main():
+    if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
 
-    elif not os.path.isfile(sys.argv[1]):
-        print("Missing {}".format(sys.argv[1]))
+    md_filename = sys.argv[1]
+    html_filename = sys.argv[2]
+
+    if not os.path.isfile(md_filename):
+        print(f"Missing {md_filename}", file=sys.stderr)
         exit(1)
 
-    else:
-        exit(0)
+    with open(md_filename, 'r') as md_file:
+        md_content = md_file.read()
+        html_content = markdown.markdown(md_content)
+
+    with open(html_filename, 'w') as html_file:
+        html_file.write(html_content)
+
+if __name__ == "__main__":
+    main()
+
